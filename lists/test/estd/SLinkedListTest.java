ce7267035d9ab7b965cb89ec288692d7696578f3
William Rozin Gaspar <williamrozingaspar@gmail.com>
2017-04-13 21:25:21 -0300

    printing a list

--- lists/test/estd/SLinkedListTest.java
+++ lists/test/estd/SLinkedListTest.java
@@ -10,7 +10,7 @@ public class SLinkedListTest {
 		SLinkedList<String> linkedList = new SLinkedList<>();
 		Assert.assertTrue(linkedList.getSize() == 0);
 		Assert.assertTrue(linkedList.isEmpty());
-		
+
 		Node<String> nodeA = new Node<>("A");
 		Node<String> nodeB = new Node<>("B");
 		Node<String> nodeC = new Node<>("C");
@@ -21,9 +21,9 @@ public class SLinkedListTest {
 
 		Assert.assertTrue(linkedList.getSize() == 3);
 		Assert.assertFalse(linkedList.isEmpty());
-		
+
 	}
-	
+
 	@Test
 	public void removeFirstTest() {
 		SLinkedList<String> linkedList = new SLinkedList<>();
@@ -35,6 +35,23 @@ public class SLinkedListTest {
 		linkedList.add(nodeB);
 		linkedList.add(nodeC);
 
-		
-	}	
+	}
+
+	@Test
+	public void printTest() {
+		SLinkedList<String> linkedList = new SLinkedList<>();
+		Node<String> nodeA = new Node<>("A");
+		Node<String> nodeB = new Node<>("B");
+		Node<String> nodeC = new Node<>("C");
+
+		linkedList.add(nodeA);
+		linkedList.add(nodeB);
+		linkedList.add(nodeC);
+
+		Node<String> element = linkedList.getFirst();
+		while (element != null) {
+			System.out.println(element.getValue());
+			element = element.getNext();
+		}
+	}
 }
